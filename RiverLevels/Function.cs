9d7534a0796a4935030153ddd041ed5495e94c49
Alun Williams <alun.williams@gmail.com>
2019-10-16 14:04:42 +0100

    BAsic functionality

--- RiverLevels/Function.cs
+++ RiverLevels/Function.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
 
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Lambda.Core;
 using System.Text.RegularExpressions;
 
@@ -21,51 +24,62 @@ namespace RiverLevels
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public void FunctionHandler(string input, ILambdaContext context)
         {
+            var url = "http://apps.sepa.org.uk/database/riverlevels/14869-SG.csv";
+            //var url = "http://mid-calder-weather.s3-website.eu-west-2.amazonaws.com/test/14869-SG.csv";
+            var wc = new System.Net.WebClient();
+            string levelsFile = wc.DownloadString(url);
 
-            //var url = "http://apps.sepa.org.uk/database/riverlevels/14869-SG.csv";
-            var url = "http://mid-calder-weather.s3-website.eu-west-2.amazonaws.com/test/14869-SG.csv";
-            var wc = new WebClient();
+            UploadLevels(ExtractLevels(levelsFile));
+        }
+        
+        private void UploadLevels(Dictionary<DateTime, double> extractedData)
+        {
+            
+            var client = new AmazonDynamoDBClient(RegionEndpoint.EUWest1);
+            
+            var table = Table.LoadTable(client, "RiverLevels");
 
-            List<string> splitted = new List<string>();
-            string fileList = wc.DownloadString(url);
-            string[] tempStr;
-
-            tempStr = fileList.Split(',');
-            var dateRegex = new Regex("\\d{1,2}\\/\\d{1,2}\\/\\d{4}");
-            var levelRegex = new Regex("^[0-9]{1,11}(?:\.[0-9]{1,3})");
-
-            var levelsDict = new Dictionary<string, string>();
-
-            foreach (string item in tempStr)
+            foreach(var row in extractedData)
             {
-                // if matches date regex, add to key part
-                if (dateRegex.IsMatch(item))
-                {
+                var item = new Document();
 
+                item["MeasurementTime"] = row.Key;
+                item["WaterLevel"] = row.Value;
 
-                }
-                // if matches levels, add to value part
-                if (dateRegex.IsMatch(item))
-                {
+                table.PutItemAsync(item);
+            }
+        }
 
+        private Dictionary<DateTime, double>  ExtractLevels(string levels)
+        {
+            var outputDict = new Dictionary<DateTime, double>();
+
+            var tempStr = levels.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            // A bit overboard to use regexes to be doubly sure the line is correct for parsing but
+            // why the devil not
+            var dateRegex = new Regex("\\d{1,2}\\/\\d{1,2}\\/\\d{4}");
+            var levelRegex = new Regex("[0-9]{1,11}(?:\\.[0-9]{1,3})");
+
+            foreach(var line in tempStr)
+            {
+                string date, level;
+
+                date = dateRegex.Match(line)?.Value;
+                level = levelRegex.Match(line)?.Value;
+
+                if(date != string.Empty && level != string.Empty){
+                    var split = line.Split(',');
+                    var measurementDateTime = split[0];
+                    var measurement = split[1];
+
+                    outputDict.Add(DateTime.Parse(measurementDateTime), double.Parse(measurement));
                 }
             }
 
-
-
-            //var client = new AmazonDynamoDBClient();
-            //var table = Table.LoadTable(client, "AnimalsInventory");
-            //var item = new Document();
-
-            //item["Mear"] = 3;
-            //item["Type"] = "Horse";
-            //item["Name"] = "Shadow";
-
-            //table.PutItem(item);
-
-            return splitted.Average(x => x[1]).ToString();
+            return outputDict;
         }
     }
 }
